2891362c17f24d82bc461acdc127c8a59a7eb38c
kuron962 <message0239@gmail.com>
2023-03-02 21:55:04 +0900

    first commit

---
+++ bookTower/src/main/java/com/kuron/bookTower/BookTowerApplication.java
@@ -0,0 +1,13 @@
+package com.kuron.bookTower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTowerApplication.class, args);
+	}
+
+}
---
+++ bookTower/src/test/java/com/kuron/bookTower/BookTowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuron.bookTower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
